4b6087f9ed8508f2c5ecfc33dd27003879996487
SondreB <sondre@outlook.com>
2025-01-19 16:30:37 +0100

    Add Font Awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,9 +16,10 @@ import { NetworkService } from './services/network.service';
           <img src="images/logo-text.svg" alt="Angor Hub Logo" class="logo">
         </a>
         <div class="nav-links">
+        <i class="fa-brands fa-bitcoin"></i>
           <select (change)="onNetworkChange($event)" [value]="networkService.getNetwork()" class="network-selector">
-            <option value="mainnet">Mainnet &#x1F4B0;</option>
-            <option value="testnet">Angor Testnet &#x1F4B0;</option>
+            <option value="mainnet">Mainnet <i class="fa-brands fa-bitcoin"></i></option>
+            <option value="testnet">Angor Testnet <i class="fa-brands fa-bitcoin"></i></option>
           </select>
           <button (click)="toggleTheme()" class="theme-toggle">
             {{ (themeService.theme$ | async) === 'light' ? '☀️' : '🌙' }}
@@ -230,6 +231,20 @@ import { NetworkService } from './services/network.service';
       margin-right: 1rem;
       padding: 0.5rem;
       font-size: 1rem;
+      appearance: none;
+      background-image: url("data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23131313%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E");
+      background-repeat: no-repeat;
+      background-position: right 0.7rem top 50%;
+      background-size: 0.65rem auto;
+      padding-right: 2rem;
+    }
+
+    [data-theme="dark"] .network-selector {
+      background-image: url("data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23ffffff%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E");
+    }
+
+    .network-selector option {
+      font-family: "Font Awesome 6 Brands", sans-serif;
     }
   `]
 })
