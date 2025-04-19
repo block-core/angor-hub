544e4a7bb1f4309da0b5a1387a30cf47571c7816
Milad Raeisi <miladsoft@yahoo.com>
2025-04-19 09:21:02 +0400

    Enhance ProjectComponent and appConfig with Markdown support for improved content rendering

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { MarkdownModule, MarkdownService } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideClientHydration(),
     provideAnimations(),
-    provideHttpClient()
+    provideHttpClient(),
+    MarkdownService,
+    ...MarkdownModule.forRoot().providers ?? []
   ]
 };
