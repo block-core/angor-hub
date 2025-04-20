17731bf9ef6c8d791fb4b13afd2879dd530b576e
Milad Raeisi <miladsoft@yahoo.com>
2025-04-20 14:53:00 +0400

    Refactor AppComponent to remove loading state management and unused imports for cleaner code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,14 @@
-import { Component, OnInit, inject, signal, Inject, PLATFORM_ID } from '@angular/core';
-import { RouterOutlet, Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { Component, OnInit, inject, Inject, PLATFORM_ID } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header.component';
 import { FooterComponent } from './components/footer.component';
-import { CommonModule } from '@angular/common';
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `
-    @if (loading()) {
-      <div class="loading-overlay">
-        <div class="spinner"></div>
-      </div>
-    }
     <app-header></app-header>
     <main>
       <router-outlet></router-outlet>
@@ -35,44 +29,15 @@ import { DOCUMENT, isPlatformBrowser } from '@angular/common';
     }
 
     main {
-      flex-grow: 1; 
-    }
-
-    .loading-overlay {
-      position: fixed;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      background: var(--background);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      z-index: 9999;
+      flex-grow: 1;
     }
   `],
 })
 export class AppComponent implements OnInit {
-  private router = inject(Router);
-
-  loading = signal(false);
-
   constructor(
     @Inject(DOCUMENT) private document: Document,
     @Inject(PLATFORM_ID) private platformId: Object
   ) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationStart) {
-        this.loading.set(true);
-      } else if (
-        event instanceof NavigationEnd ||
-        event instanceof NavigationCancel ||
-        event instanceof NavigationError
-      ) {
-        setTimeout(() => this.loading.set(false), 100); 
-      }
-    });
-
     if (isPlatformBrowser(this.platformId)) {
       try {
         const savedTheme = localStorage.getItem('angor-theme');
