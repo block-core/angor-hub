b1d07bfcf29a9e4954d0514a23952d8f309e149c
yaya <181195712+Zazawowow@users.noreply.github.com>
2026-09-13 20:35:58 +0200

    style: unify dark surfaces and refresh Hub branding

--- src/app/components/app-launcher.component.ts
+++ src/app/components/app-launcher.component.ts
@@ -9,7 +9,7 @@ import { RouterLink } from '@angular/router';
   template: `
     <div class="relative">
       <a routerLink="/" class="cursor-pointer group bg-transparent border-0 p-0 flex items-center gap-2 no-underline" title="Angor Home">
-        <img src="/images/app-icon-dark-mode.png" alt="Angor" class="h-8 w-auto transition-all duration-300 group-hover:scale-105 group-hover:brightness-125">
+        <img src="/images/app-icon.png" alt="" width="32" height="32" class="h-8 w-8 transition-all duration-300 group-hover:scale-105 group-hover:brightness-125">
         <span class="text-header-text font-semibold text-lg transition-all duration-300 group-hover:text-accent">Angor</span>
       </a>
     </div>
